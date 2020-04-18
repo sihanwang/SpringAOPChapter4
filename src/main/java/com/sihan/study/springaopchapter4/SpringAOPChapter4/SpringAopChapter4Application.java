cc1118fb9f8dee947ba3f30f43964797017ed215
sihanwang <ocpwj@hotmail.com>
2020-04-18 11:22:45 +0800

    init

---
+++ src/main/java/com/sihan/study/springaopchapter4/SpringAOPChapter4/SpringAopChapter4Application.java
@@ -0,0 +1,13 @@
+package com.sihan.study.springaopchapter4.SpringAOPChapter4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAopChapter4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAopChapter4Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/sihan/study/springaopchapter4/SpringAOPChapter4/SpringAopChapter4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sihan.study.springaopchapter4.SpringAOPChapter4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAopChapter4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
